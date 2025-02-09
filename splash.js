ce51f862faa7071088806eb9e06ab1653db4c38c
MReiss97 <maxreiss21@gmail.com>
2025-02-08 23:04:13 -0500

    added bg-color, added navbar, fixed custom cursor

--- splash.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('video');
-
-    // Wait for the video to finish playing
-    video.onended = function() {
-        // Fade out the splash screen
-        document.querySelector('.splash').classList.add('fade-out');
-    };
-});
